d6d094151c695f3a99c34d22595cd049c03ca365
albin <albinguignabert@gmail.com>
2025-12-19 14:06:54 +0100

    feat: add database connection

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
 import "fmt"
+import "os"
+import "log"
+import (
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
 
 func main() {
-    fmt.Println("Hello, World!")
+
+	dsn := "host=localhost user=" + os.Getenv("POSTGRES_USER") +
+		" password=" + os.Getenv("POSTGRES_PASSWORD") +
+		" dbname=" + os.Getenv("POSTGRES_DB") +
+		" port=5432 sslmode=disable TimeZone=Europe/Paris"
+
+	_, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("Impossible de se connecter à la DB : %v", err)
+	}
+
+	fmt.Println("Connexion à PostgreSQL réussie avec GORM !")
 }
